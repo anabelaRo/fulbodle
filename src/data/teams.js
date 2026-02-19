c04e323734a87cb9bcd798b4498cf3d1a533f7ab
Anabela <anabela.rossi@gmail.com>
2026-02-19 17:16:55 -0300

    Create teams.js

---
+++ src/data/teams.js
@@ -0,0 +1,24 @@
+// src/data/teams.js
+export const teams = [
+  {
+    id: 1,
+    nombre: "River Plate",
+    pais: "Argentina",
+    federacion: "CONMEBOL",
+    categoria: "A",
+    colores: ["Blanco", "Rojo"],
+    palmares: 38, // Títulos de liga local
+    fundacion: 1901
+  },
+  {
+    id: 2,
+    nombre: "Chacarita Juniors",
+    pais: "Argentina",
+    federacion: "CONMEBOL",
+    categoria: "B",
+    colores: ["Rojo", "Negro", "Blanco"],
+    palmares: 1,
+    fundacion: 1906
+  },
+  // ... añadir más equipos aquí
+];
